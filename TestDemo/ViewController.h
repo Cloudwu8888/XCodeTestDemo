a963791a2bcb06dce7c4c33d60ad917e4f22727e
bys <bys@bysdembp-2.guest.corp.microsoft.com>
2017-06-05 18:18:10 +0800

    Initial Commit

---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by bys on 05/06/2017.
+//  Copyright © 2017 bys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestDemo
+//
+//  Created by bys on 05/06/2017.
+//  Copyright © 2017 bys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
